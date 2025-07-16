f223ed239843736eb9a211bbe5d3019f460f29ba
Ritik Nagpure <80842165+Ritik-Nagpure@users.noreply.github.com>
2025-07-16 21:16:17 +0530

    new component

--- src/LandingPage/Projects/LinkMaster.tsx
+++ src/LandingPage/Projects/LinkMaster.tsx
@@ -1,33 +1,47 @@
 import { useState } from "react";
 import { projectDetails } from "./ProjectDetails";
+import './LinkMaster.css';
 
 const LinkMaster = () => {
-    const [ diplayApp, setDisplayApp ] = useState(0);
+    const [displayApp, setDisplayApp] = useState(0);
 
     const changeDisplay = (index: number) => {
         setDisplayApp(index);
     }
 
-    const createDisplayProject = (index : number) => {
+    const createDisplayProject = (index: number) => {
         return (
-            <div key={index} className="project-display">
-                <h2>{projectDetails[index].title}</h2>
-                <p>{projectDetails[index].desc}</p>
-                <a href={projectDetails[index].link} target="_blank" rel="noopener noreferrer">View Project</a>
+            <div
+                className="relative w-full h-full shrink-0 rounded-lg overflow-hidden mx-4 group"
+                style={{
+                    backgroundImage: `url(${projectDetails[index].img})`,
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'top',
+                }}>
+                <div className="absolute bottom-0 w-full sm:h-1/2 bg-black/30 text-white p-4 flex flex-col justify-end ">
+                    <a href={projectDetails[index].link}>
+                        <span className="text-6xl font-semibold">{projectDetails[index].title}</span>
+                    </a>
+                    <br />
+                    <br />
+                    <p className="text-lg overflow-hidden">{projectDetails[index].desc}</p>
+                </div>
             </div>
+
         );
     }
 
     return (
-        <div className="flex flex-row justifty-between items-center sm:p-5 sm:m-5 bg-red-500 sm:h-150">
-            <div className="w-1/6 h-full flex flex-col end justify-between items-center gap-4 bg-green-300">
-                <div>Astrobit</div>
-                <div>Hable</div>
-                <div>Kiracar</div>
-                <div>CodeGears</div>
+        <div className="flex flex-row justifty-between items-center sm:p-5 sm:m-5 sm:h-150">
+            <div className="w-1/6 h-full flex flex-col end justify-between items-center gap-4">
+                <button className={"cursor-pointer app-link-button" + displayApp === 0 && ""}  onClick={() => changeDisplay(0)}><span>Kiracar</span></button>
+                <button className="cursor-pointer app-link-button" onClick={() => changeDisplay(1)}>Astrobit</button>
+                <button className="cursor-pointer app-link-button" onClick={() => changeDisplay(2)}>Habla</button>
+                <button className="cursor-pointer app-link-button" onClick={() => changeDisplay(3)}>CodeGears</button>
             </div>
-            <div className="w-5/6 h-full bg-blue-300">
-                Project Data here
+            <div className="w-5/6 h-full">
+                {createDisplayProject(displayApp)}
             </div>
         </div>
     )
